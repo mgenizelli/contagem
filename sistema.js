a526dbd8212d4e4e8efadf0d834701ee2d0d3a3c
mgenizelli <mgenizelli@outlook.com>
2021-09-07 00:34:42 -0300

    adicionando atividade de contagem ao espaço remoto

---
+++ sistema.js
@@ -0,0 +1,27 @@
+let numeroDeAlunos = 10;
+
+
+
+ for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+      console.log(contador);
+ if ( contador === 0) {
+      console.log("O número é ZERO");
+       } else if  (contador % 2 ==0 && contador > 5 ) {
+        
+       console.log("O número atual é " + contador + " é PAR E MAIOR QUE 5 ");
+
+     } else {
+          console.log ("O número atual é " + contador + " é IMPAR");
+    
+      }
+
+    }
+
+
+     /* 
+
+     AND (E)
+     OR   (OU)
+     NOT   (NÃO)
+            
+     */
